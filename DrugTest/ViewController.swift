c47150cfceddbb674841e44e37d6b6a0a2fe90f4
大久保将博 <ookubomasahiro@ookubosusumuhakunoMacBook-Pro.local>
2017-01-29 23:20:55 +0900

    Initial Commit

---
+++ DrugTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrugTest
+//
+//  Created by 大久保将博 on 2017/01/29.
+//  Copyright © 2017年 大久保将博. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

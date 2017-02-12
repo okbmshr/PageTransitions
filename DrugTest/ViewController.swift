d28ce593c68cf551430bd352ca436135f1d74922
masahiro-okubo <okb.nst@gmail.com>
2017-02-12 18:40:08 +0900

    version001

--- DrugTest/ViewController.swift
+++ DrugTest/ViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func panLabel(_ sender: UIPanGestureRecognizer) {
+        //移動量を取得する。
+        let move:CGPoint = sender.translation(in: view)
+        
+        //ドラッグした部品の座標に移動量を加算する。
+        sender.view!.center.x += move.x
+        sender.view!.center.y += move.y
+        
+        //ラベルに現在座標を表示する。
+        //testLabel.text = "\(sender.view!.frame.origin.x), \(sender.view!.frame.origin.y)"
+        
+        //移動量を0にする。
+        sender.setTranslation(CGPoint.zero, in: view)
     }
+    
+    
+
 
 
 }

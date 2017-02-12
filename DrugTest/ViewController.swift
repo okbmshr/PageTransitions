fead341cc07e6121ba68fb470e690851775ddf66
masahiro-okubo <okb.nst@gmail.com>
2017-02-12 20:10:29 +0900

    dropzone追加

--- DrugTest/ViewController.swift
+++ DrugTest/ViewController.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var testLabel: UILabel!
+    
+    //移動対象のオブジェクト
+    @IBOutlet weak var testlabel_1: UILabel!
+    @IBOutlet weak var testlabel_2: UILabel!
+    @IBOutlet weak var testlabel_3: UILabel!
+    
+    // ドロップゾーンのオブジェクト
+    @IBOutlet weak var dropzone_1: UIView!
+    @IBOutlet weak var dropzone_2: UIView!
+    @IBOutlet weak var dropzone_3: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    //ラベルA
+    @IBAction func panLabel_1(_ sender: UIPanGestureRecognizer) {
+        panlabel_move(sender)
+    }
     
-    @IBAction func panLabel(_ sender: UIPanGestureRecognizer) {
+    //ラベルB
+    @IBAction func panLabel_2(_ sender: UIPanGestureRecognizer) {
+        panlabel_move(sender)
+    }
+    
+    //ラベルC
+    @IBAction func panLabel_3(_ sender: UIPanGestureRecognizer) {
+        panlabel_move(sender)
+    }
+    
+    //ラベルを移動する
+    func panlabel_move(_ sender: UIPanGestureRecognizer) {
         //移動量を取得する。
         let move:CGPoint = sender.translation(in: view)
         
@@ -26,16 +49,28 @@ class ViewController: UIViewController {
         sender.view!.center.x += move.x
         sender.view!.center.y += move.y
         
-        //ラベルに現在座標を表示する。
-        //testLabel.text = "\(sender.view!.frame.origin.x), \(sender.view!.frame.origin.y)"
-        
         //移動量を0にする。
         sender.setTranslation(CGPoint.zero, in: view)
     }
     
     
-
-
+    
+//    @IBAction func panLabel(_ sender: UIPanGestureRecognizer) {
+//        //移動量を取得する。
+//        let move:CGPoint = sender.translation(in: view)
+//        
+//        //ドラッグした部品の座標に移動量を加算する。
+//        sender.view!.center.x += move.x
+//        sender.view!.center.y += move.y
+//        
+//        //ラベルに現在座標を表示する。
+//        //testLabel.text = "\(sender.view!.frame.origin.x), \(sender.view!.frame.origin.y)"
+//        
+//        //移動量を0にする。
+//        sender.setTranslation(CGPoint.zero, in: view)
+//    }
+    
 
 }
+
 
